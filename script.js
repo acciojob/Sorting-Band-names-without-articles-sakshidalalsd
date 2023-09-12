c772f56c38039297165a9d9de19ced60ee926406
sakshidalalsd <139738505+sakshidalalsd@users.noreply.github.com>
2023-09-12 19:34:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-//your code here
+let arr= ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+let regpex=/\b the\b | \b an \b | \b a \b /gi;
+let article=[];
+let map={};
+for(let i=0;i<arr.length;i++){
+	let art=arr[i].replace(regpex ,"").trim();
+	article.push(art);
+	map[art]=arr[i];
+}
+  article.sort();
+  let ans=[];
+ for(let j=0;j<article.length;j++){
+	 ans.push(mp[i]);
+ }
+console.log(ans);
+
 
